5126f52ed66bd2e9e2e0e1258552b9afc997225d
Sean Cannon <alienwebguy@gmail.com>
2016-10-18 10:03:07 -0700

    fix some styling and JS for iOS

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -4,25 +4,27 @@
   const anchors = document.getElementsByTagName('A');
 
   const removeActiveClassFromAnchors = () => {
-    for (let a of anchors) {
+    [].forEach.call(anchors, (a) => {
       a.classList.remove('active');
-    }
+    });
   };
 
-  for (let a of anchors) {
+  [].forEach.call(anchors, (a) => {
     a.addEventListener('click', (e) => {
       removeActiveClassFromAnchors();
       e.target.classList.add('active');
-    })
-  }
+    });
+  });
 
   window.addEventListener('hashchange', (e) => {
-    document.querySelector('a[href="' + window.location.hash + '"]').click();
+    window.setTimeout(() => {
+      document.querySelector('a[href="' + window.location.hash + '"]').click();
+    }, 100);
   });
 
   if (!window.location.hash) {
     window.location = '#background';
-    window.dispatchEvent(new HashChangeEvent("hashchange"))
+    window.dispatchEvent(new HashChangeEvent("hashchange"));
   } else {
     document.querySelector('a[href="' + window.location.hash + '"]').click();
   }
